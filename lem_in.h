c6ca77e8a9c933d72229643e5291e512fadadf8c
johnhasgone <eu.vaganov@gmail.com>
2020-06-28 15:48:27 +0300

    added set_weight, create_edge, clone_room, duplicate_room, edge_struct, and made some refactoring

--- lem_in.h
+++ lem_in.h
@@ -15,17 +15,34 @@
 
 #include "libft/libft.h"
 
+/* Edge of an ant farm
+ * weight: 1, 0, -1 										  //something about the direction of yhe edge
+ * from: position of the room in an ant farm (current room) - //or delete this field?
+ * to: position of the room current room is connected to
+ * */
+typedef struct		s_edge
+{
+	int 			weight;
+	int				from;
+	int				to;
+}					t_edge;
+
+// Node of an ant farm
 typedef struct		s_room
 {
 	char			*name;
 	int 			coordinates[2];
-	int				edges[10000];
+	t_edge			*edges[10000];
 	int				deg;
 	int 			type;	// 0 - ordinary, 1 - start, 2 - finish
-	int 			way;
+	int 			way;    // 1 - included in a way for ants, 0 - not included
 }					t_room;
 
-int line_analyzer_room(char *line, t_room **farm, int *i, int type);
-int		line_analyzer_edge(char *line, t_room **farm, int count);
+
+int		analyze_room_line(char *line, t_room **farm, int *room_counter, int type);
+int		analyze_edge_line(char *line, t_room **farm, int room_counter);
+void		set_weight(t_edge **edge, int weight);
+t_edge		*create_edge(int from, int to);
+
 
 #endif
--- main.c
+++ main.c
@@ -12,7 +12,8 @@
 
 #include "lem_in.h"
 
-int			int_check(char *str)
+
+int			check_integer(char *str)
 {
 	int		i;
 	int		num;
@@ -41,76 +42,96 @@ int			int_check(char *str)
 	return (1);
 }
 
-int line_analyzer_edge(char *line, t_room **farm, int count)
+/*checking if the line contains enough information for creating an edge and creating it*/
+int analyze_edge_line(char *line, t_room **farm, int room_counter)
 {
-	char	**edge;
+	char	**edge_line;
 	int		i;
 	int 	j;
+	t_edge	*edge_from;
+	t_edge	*edge_to;
 
 	i = 0;
 	j = 0;
-	edge = ft_strsplit(line, '-');
-	while (i < count)
+	edge_line = ft_strsplit(line, '-');
+	while (i < room_counter)
 	{
-		if (ft_strcmp(edge[0], farm[i]->name) == 0)
+		if (ft_strcmp(edge_line[0], farm[i]->name) == 0)
 			break ;
 		i++;
 	}
-	while (j < count)
+	while (j < room_counter)
 	{
-		if (ft_strcmp(edge[1], farm[j]->name) == 0)
+		if (ft_strcmp(edge_line[1], farm[j]->name) == 0)
 			break ;
 		j++;
 	}
-	if (i != count && j != count)
+	if (i != room_counter && j != room_counter)
 	{
-		(farm[i]->edges)[(farm[i]->deg)++] = j;
-		(farm[j]->edges)[(farm[j]->deg)++] = i;
+		edge_from = create_edge(i, j);
+		set_weight(&edge_from, 1);
+		edge_to = create_edge(j, i);
+		set_weight(&edge_to, 1);
+		(farm[i]->edges)[(farm[i]->deg)++] = edge_from;
+		(farm[j]->edges)[(farm[j]->deg)++] = edge_to;
 		return (1);
 	}
 	return (0);
 }
 
-int			line_analyzer(char **line, int *ants, t_room **farm, int *i)
+/* setting weight of an edge */
+void		set_weight(t_edge **edge, int weight) {
+	(*edge)->weight = weight;
+}
+
+/* creating a new edge between rooms */
+t_edge		*create_edge(int from, int to) {
+	t_edge *edge;
+
+	edge = (t_edge*)malloc(sizeof(t_edge));
+	edge->from = from;
+	edge->to = to;
+	return edge;
+}
+
+/*
+** checking a line on meeting the requirements
+** correct room coordinates, edges, comments return 1
+** all the other lines return 0
+*/
+int			analyze_line(char **line, int *ants, t_room **farm, int *room_counter)
 {
 	if (ft_countrows(*line, ' ') == 3)
-	{
-		if (!line_analyzer_room(*line, farm, i, 0))
-			return (0);
-	}
+		return (analyze_room_line(*line, farm, room_counter, 0));
 	else if (ft_countrows(*line, '-') == 2)
-	{
-		if (!line_analyzer_edge(*line, farm, *i))
-			return (0);
-	}
+		return (analyze_edge_line(*line, farm, *room_counter));
 	else if (ft_strcmp(*line, "##start") == 0)
 	{
 		get_next_line(0, line);
-		if (!line_analyzer_room(*line, farm, i, 1))
-			return (0);
+		return (analyze_room_line(*line, farm, room_counter, 1));
 	}
 	else if (ft_strcmp(*line, "##end") == 0)
 	{
 		get_next_line(0, line);
-		if (!line_analyzer_room(*line, farm, i, 2))
-			return (0);
+		return (analyze_room_line(*line, farm, room_counter, 2));
 	}
-	else if (int_check(*line) == 1)
+	else if (check_integer(*line) == 1)
+	{
 		*ants = ft_atoi(*line);
+		return (1);
+	}
 	else if (*line[0] == '#')
 		return (1);
-	else
-		return (0);
-	return (1);
+	return (0);
 }
 
-int line_analyzer_room(char *line, t_room **farm, int *i, int type)
+int analyze_room_line(char *line, t_room **farm, int *room_counter, int type)
 {
 	t_room	*room;
 	char 	**room_and_coord;
 
 	room_and_coord = ft_strsplit(line, ' ');
-	if (int_check(room_and_coord[1]) && int_check(room_and_coord[2]))
+	if (check_integer(room_and_coord[1]) && check_integer(room_and_coord[2]))
 	{
 		room = (t_room*)malloc(sizeof(t_room));
 		room->name = room_and_coord[0];
@@ -118,11 +139,27 @@ int line_analyzer_room(char *line, t_room **farm, int *i, int type)
 		room->coordinates[1] = ft_atoi(room_and_coord[2]);
 		room->deg = 0;
 		room->type = type;
-		farm[(*i)++] = room;
+		farm[(*room_counter)++] = room;
 		return (1);
 	}
-	else
-		return (0);
+	return (0);
+}
+
+t_room *clone_room(t_room *room)
+{
+	t_room	*clone_room;
+
+	clone_room = (t_room*)malloc(sizeof(t_room));
+	clone_room->name = room->name;
+	clone_room->coordinates[0] = room->coordinates[0];
+	clone_room->coordinates[1] = room->coordinates[1];
+	clone_room->deg = room->deg;
+	clone_room->type = room->type;
+}
+
+void duplicate_rooms(t_room **farm)
+{
+
 }
 
 int main()
@@ -130,13 +167,14 @@ int main()
 	char 		*line;
 	t_room		*farm[10000];
 	int			ants;
-	int 		i;
+	int 		room_counter;
 
 	ants = -1;
-	i = 0;
-	while (get_next_line(0, &line))
-	{
-		if (!line_analyzer(&line, &ants, farm, &i))
-			write(2, "Incorrect farm instructions\n", 28);
+	room_counter = 0;
+	while (get_next_line(0, &line)) {
+        if (!analyze_line(&line, &ants, farm, &room_counter)) {
+            write(2, "Incorrect farm instructions\n", 28);
+            exit(1);
+        }
 	}
 }
